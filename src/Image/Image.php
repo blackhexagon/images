ee984da3d482879c587f56a6b3c5fa328f162a36
Matous Trca <matous.trca@gmail.com>
2020-12-10 10:02:53 +0100

    revert file extension change

--- src/Image/Image.php
+++ src/Image/Image.php
@@ -16,20 +16,20 @@ class Image extends \Nette\Utils\Image implements IImage {
     ];
 
     /** @var int|null */
-	private $quality;
+    private $quality;
 
     /** @var int|null */
     private $type;
 
-	/**
-	 * @param int|null $quality
-	 * @return static
-	 */
-	public function setQuality(?int $quality): self {
-		$this->quality = $quality;
+    /**
+     * @param int|null $quality
+     * @return static
+     */
+    public function setQuality(?int $quality): self {
+        $this->quality = $quality;
 
-		return $this;
-	}
+        return $this;
+    }
 
     /**
      * @param int|null $type
@@ -41,43 +41,30 @@ class Image extends \Nette\Utils\Image implements IImage {
         return $this;
     }
 
-	/**
-	 * Saves image to the file.
-	 * @param string $file  filename
-	 * @param int|null $quality  quality (0..100 for JPEG and WEBP, 0..9 for PNG)
-	 * @param int $type  optional image type
-	 */
-	public function save(string $file, int $quality = null, int $type = null): void {
-        parent::save($this->changeFileExtension($file), $quality === null ? $this->quality : $quality, $type === null ? $this->type : $type);
+    /**
+     * Saves image to the file.
+     * @param string $file  filename
+     * @param int|null $quality  quality (0..100 for JPEG and WEBP, 0..9 for PNG)
+     * @param int $type  optional image type
+     */
+    public function save(string $file, int $quality = null, int $type = null): void {
+        parent::save($file, $quality === null ? $this->quality : $quality, $type === null ? $this->type : $type);
     }
 
-	/**
-	 * @param string $file
-	 *
-	 * @return string
-	 */
-	private function changeFileExtension(string $file): string {
-	    if (!$this->type)
-	        return $file;
-	    $newExtension = Image::typeToExtension($this->type);
-	    $oldExtension = Image::typeToExtension(Image::detectTypeFromFile($file));
-		return str_replace($oldExtension, $newExtension, $file);
-	}
+    /**
+     * @param IResource|string $resource
+     *
+     * @return int
+     * @throws \Nette\InvalidArgumentException
+     */
+    public static function getImageType($resource): int {
+        $resource = $resource instanceof IResource ? $resource->getName() : $resource;
+        $extensions = self::EXTENSIONS;
+        if (!isset($extensions[$extension = strtolower(pathinfo($resource, PATHINFO_EXTENSION))])) {
+            throw new InvalidArgumentException("Unsupported file extension '$extension'.");
+        }
 
-	/**
-	 * @param IResource|string $resource
-	 *
-	 * @return int
-	 * @throws \Nette\InvalidArgumentException
-	 */
-	public static function getImageType($resource): int {
-		$resource = $resource instanceof IResource ? $resource->getName() : $resource;
-		$extensions = self::EXTENSIONS;
-		if (!isset($extensions[$extension = strtolower(pathinfo($resource, PATHINFO_EXTENSION))])) {
-			throw new InvalidArgumentException("Unsupported file extension '$extension'.");
-		}
-
-		return $extensions[$extension];
-	}
+        return $extensions[$extension];
+    }
 
 }
